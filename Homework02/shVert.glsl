78f2560d1108b417ae7a759eb231ebf4e2ed36a9
Jo-Wonhee <60100293+Jo-Wonhee@users.noreply.github.com>
2025-09-19 22:34:43 +0900

    Update shVert.glsl

--- Homework02/shVert.glsl
+++ Homework02/shVert.glsl
@@ -5,8 +5,5 @@ in vec3 aPos;
 uniform vec2 uOffset;
 
 void main() {
-    gl_Position = vec4(
-        clamp(aPos[0]+uOffset[0],-0.9,0.9),
-        clamp(aPos[1]+uOffset[1],-0.9,0.9),
-        aPos[2], 1.0);
+    gl_Position = vec4(aPos[0]+uOffset[0], aPos[1]+uOffset[1], aPos[2], 1.0);
 } 
