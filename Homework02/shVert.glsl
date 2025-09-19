27a06674102acb82b315d9ab256d7a937406ab04
Jo-Wonhee <60100293+Jo-Wonhee@users.noreply.github.com>
2025-09-19 21:37:50 +0900

    Update shVert.glsl

--- Homework02/shVert.glsl
+++ Homework02/shVert.glsl
@@ -2,8 +2,8 @@
 
 layout (location = 0) in vec3 aPos;
 
-uniform float verticalFlip;
+uniform vec2 uOffset;
 
 void main() {
-    gl_Position = vec4(aPos[0], aPos[1] * verticalFlip, aPos[2], 1.0);
+    gl_Position = vec4(aPos[0]+uOffset[0], aPos[1]+uOffset[1], aPos[2], 1.0);
 } 
